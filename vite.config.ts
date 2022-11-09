2275920198d96c02e4fba51b121468610477d2e0
Chris Foss <chrisjfoss@gmail.com>
2022-11-09 00:03:29 -0500

    Removed aliasing for electron folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import { rmSync } from 'fs'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-electron-plugin'
-import { customStart, loadViteEnv } from 'vite-electron-plugin/plugin'
-import renderer from 'vite-plugin-electron-renderer'
-import pkg from './package.json'
-import { resolve } from 'path';
+import { rmSync } from 'fs';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-electron-plugin';
+import { customStart, loadViteEnv } from 'vite-electron-plugin/plugin';
+import renderer from 'vite-plugin-electron-renderer';
+import pkg from './package.json';
+import path from 'path';
 
-rmSync('dist-electron', { recursive: true, force: true })
+rmSync('dist-electron', { recursive: true, force: true });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      '%': resolve(__dirname, './electron')
+      '@': path.resolve(__dirname, './src'),
     }
   },
   plugins: [
@@ -58,7 +57,7 @@ export default defineConfig({
       }
     }
   }
-})
+});
 
 function debounce<Fn extends (...args: any[]) => void>(fn: Fn, delay = 299) {
   let t: NodeJS.Timeout
@@ -66,4 +65,4 @@ function debounce<Fn extends (...args: any[]) => void>(fn: Fn, delay = 299) {
     clearTimeout(t)
     t = setTimeout(() => fn(...args), delay)
   }) as Fn
-}
+};
