0612559ca6b47a1b9e1331d2ff52043512c2b6e3
Chris Foss <chrisjfoss@gmail.com>
2022-11-12 01:32:10 -0500

    Created file for alert dialog
    Fixed some linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,56 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: [
+    '@typescript-eslint',
+    'vue',
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    createDefaultProgram: true,
+    project: [
+      './tsconfig.json',
+    ],
   },
+  overrides: [
+    {
+      files: ['./src/electron'],
+      parserOptions: {
+        project: ['./tsconfig.node.json'],
+      },
+    },
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "prettier",
-    "@vue/typescript",
+    'eslint:recommended',
+    'google',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:vue/recommended',
+    'plugin:vuejs-accessibility/recommended',
+    "eslint-config-prettier",
   ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      alias: {
+        map: [['@/', './src/']],
+      },
+      extensions: [
+        '.js', '.ts', '.vue',
+      ],
+    },
+  },
 };
---
+++ src/renderer/components/AlertDialog/AlertDialog.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+    show: Boolean
+})
+
+const emit = defineEmits(['update:show']);
+
+
+</script>
+<template>
+    <q-dialog v-model="show">
+        <q-card>
+
+        </q-card>
+    </q-dialog>
+</template>
+<style lang="scss">
+
+</style>
